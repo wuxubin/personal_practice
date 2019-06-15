d2a7415150a2f17e7d55adff3d1f7363acbae087
guafe <1265480781@qq.com>
2019-06-15 11:01:32 +0800

    文件夹

--- game/main.js
+++ game/main.js
@@ -35,9 +35,9 @@ var enableDebugMode = function (game, enable) {
 
 var _main = function () {
     var images = {
-        ball: 'ball.png',
-        block: 'block.png',
-        paddle: 'paddle.png',
+        ball: 'img/ball.png',
+        block: 'img/block.png',
+        paddle: 'img/paddle.png',
     }
     window.fps = 60
     var game = Guagame(images, function (g) {
