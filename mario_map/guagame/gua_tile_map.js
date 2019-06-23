cf1a466e804e914956e7795834d519da3f899b61
guafe <1265480781@qq.com>
2019-06-23 21:51:11 +0800

    mario

--- mario_map/guagame/gua_tile_map.js
+++ mario_map/guagame/gua_tile_map.js
@@ -1,10 +1,26 @@
 class GuaTileMap {
     constructor(game) {
         this.game = game
+        this.offsetX = 10
         this.tiles = [
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2,
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+            4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+            4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+            4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+            4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+            4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+            4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+            4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+            4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+            4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+            4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+            4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+            4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+            4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+            4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
+            4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
             4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
             4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
             4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,
@@ -29,14 +45,20 @@ class GuaTileMap {
         return tile != 0
     }
     update() {
-
+        this.offsetX -= 1
     }
     draw() {
         let h = this.th
-        for (let i = 0; i < this.tiles.length; i++) {
+        let offsetIndex = Math.abs(parseInt(this.offsetX / this.tileSize))
+        let numberOfTiles = h * (12 + 1)
+        if (offsetIndex + numberOfTiles < this.tiles.length) {
+            numberOfTiles = this.tiles.length
+        }
+        for (let i = offsetIndex; i < numberOfTiles; i++) {
             const index = this.tiles[i];
             if (index != 0) {
                 let x = Math.floor(i / h) * this.tileSize
+                x += this.offsetX
                 let y = (i % h) * this.tileSize
                 let image = this.tileImages[index - 1]
                 this.game.context.drawImage(image.texture, x, y)
