beeb59a31726331d42c9743047bcefd2517fac46
guafe <1265480781@qq.com>
2019-07-16 14:22:41 +0800

    测试

--- node/server.js
+++ node/server.js
@@ -1,37 +1,25 @@
 var express = require('express');
 var app = express();
-var fs = require("fs");
- 
 var bodyParser = require('body-parser');
-var multer  = require('multer');
+ 
+// 创建 application/x-www-form-urlencoded 编码解析
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
  
 app.use('/public', express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ dest: '/tmp/'}).array('image'));
  
 app.get('/index.htm', function (req, res) {
    res.sendFile( __dirname + "/" + "index.htm" );
 })
  
-app.post('/file_upload', function (req, res) {
+app.post('/process_post', urlencodedParser, function (req, res) {
  
-   console.log(req.files[0],req.query);  // 上传的文件信息
- 
-   var des_file = __dirname + "/" + req.files[0].originalname;
-   fs.readFile( req.files[0].path, function (err, data) {
-        fs.writeFile(des_file, data, function (err) {
-         if( err ){
-              console.log( err );
-         }else{            
-               response = {
-                   message:'File uploaded successfully', 
-                   filename:req.files[0].originalname
-              };
-          }
-        //   console.log( response );
-        //   res.end( JSON.stringify( response ) );
-       });
-   });
+   // 输出 JSON 格式
+   var response = {
+       "first_name":req.body.first_name,
+       "last_name":req.body.last_name
+   };
+   console.log(req.body,req.files);
+   // res.end(JSON.stringify(response));
 })
  
 var server = app.listen(8081, function () {
