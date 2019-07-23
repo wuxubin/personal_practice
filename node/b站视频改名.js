610d423eee2372e34de9e7daa6ced101dc37c68b
wuxubin <43275249+wuxubin@users.noreply.github.com>
2019-07-23 22:58:40 +0800

    Update b站视频改名.js

--- node/b站视频改名.js
+++ node/b站视频改名.js
@@ -1,6 +1,6 @@
 var path = require("path");
 var fs = require("fs");
-var num = 52417143
+var num = 52545607
 for (let i = 0; i < 100; i++) {
     let pathName = `D:/download/${num}/${i + 1}/entry.json`;
     fs.readFile(pathName, function (err, data) {
@@ -9,13 +9,13 @@ for (let i = 0; i < 100; i++) {
         }
         for (let j = 0; j < 50; j++) {
             let d = JSON.parse(data).page_data.part;
-            let path1 = `D:/download/${num}/${i + 1}/lua.flv.bili2api.80/${j}.blv`
+            let path1 = `D:/download/${num}/${i + 1}/lua.flv720.bili2api.64/${j}.blv`
             let path2 = `D:/download/${num}/${i}-${d}${j}.mp4`
             fs.rename(path1, path2, function (err) {
-                if (err) {
+                if (err) {                    
                     return;
                 }
-                // console.log('重命名成功', i + 1);
+                console.log('重命名成功', i + 1);
             });
         }
     });
