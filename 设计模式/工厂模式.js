ca701b2a5efcf02482429132b4cf0dd56120c3ba
wuxubin <43275249+wuxubin@users.noreply.github.com>
2019-07-23 23:11:11 +0800

    Create 工厂模式.js

---
+++ 设计模式/工厂模式.js
@@ -0,0 +1,25 @@
+class Product {
+    constructor(name) {
+        this.name = name
+    }
+    init() {
+        alert('init')
+    }
+    fun1() {
+        alert('fun1')
+    }
+    fun2() {
+        alert('fun2')
+    }
+}
+
+class Creator {
+    create(name) {
+        return new Product(name)
+    }
+}
+//测试
+let creator = new Creator()
+let p = creator.create('p1')
+p.init()
+p.fun1()
