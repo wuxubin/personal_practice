1cabff84cf1ef716818a51dcbf79fa74f12d910f
wuxubin <43275249+wuxubin@users.noreply.github.com>
2019-07-23 23:36:35 +0800

    Update 装饰器模式.js

--- 设计模式/装饰器模式.js
+++ 设计模式/装饰器模式.js
@@ -1,1 +1,23 @@
-// 先跳过
+class Circle {
+    draw() {
+        console.log("画一个圆形");
+    }
+}
+class Decorator {
+    constructor(circle) {
+        this.circle = circle;
+    }
+    draw() {
+        this.circle.draw();
+        this.setRedBorder(circle);
+    }
+    setRedBorder(circle) {
+        console.log("设置红色边框");
+    }
+}
+
+const circle = new Circle();
+circle.draw();
+
+const dec = new Decorator(circle);
+dec.draw();
