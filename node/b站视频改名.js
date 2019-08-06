a3b6756f351f3f4a961657388db315b2568d0752
wuxubin <43275249+wuxubin@users.noreply.github.com>
2019-08-06 22:46:14 +0800

    Update b站视频改名.js

--- node/b站视频改名.js
+++ node/b站视频改名.js
@@ -1,6 +1,29 @@
+// var path = require("path");
+// var fs = require("fs");
+// var num = 52545607
+// for (let i = 0; i < 100; i++) {
+//     let pathName = `D:/download/${num}/${i + 1}/entry.json`;
+//     fs.readFile(pathName, function (err, data) {
+//         if (err) {
+//             return;
+//         }
+//         for (let j = 0; j < 50; j++) {
+//             let d = JSON.parse(data).page_data.part;
+//             let path1 = `D:/download/${num}/${i + 1}/lua.flv720.bili2api.64/${j}.blv`
+//             let path2 = `D:/download/${num}/${i}-${d}${j}.mp4`
+//             fs.rename(path1, path2, function (err) {
+//                 if (err) {                    
+//                     return;
+//                 }
+//                 console.log('重命名成功', i + 1);
+//             });
+//         }
+//     });
+// }
+
 var path = require("path");
 var fs = require("fs");
-var num = 52545607
+var num = 56394225
 for (let i = 0; i < 100; i++) {
     let pathName = `D:/download/${num}/${i + 1}/entry.json`;
     fs.readFile(pathName, function (err, data) {
@@ -9,8 +32,8 @@ for (let i = 0; i < 100; i++) {
         }
         for (let j = 0; j < 50; j++) {
             let d = JSON.parse(data).page_data.part;
-            let path1 = `D:/download/${num}/${i + 1}/lua.flv720.bili2api.64/${j}.blv`
-            let path2 = `D:/download/${num}/${i}-${d}${j}.mp4`
+            let path1 = `D:/download/${num}/${i + 1}/16/audio.m4s`
+            let path2 = `D:/download/${num}/${i}-${d}${j}.mp3`
             fs.rename(path1, path2, function (err) {
                 if (err) {                    
                     return;
