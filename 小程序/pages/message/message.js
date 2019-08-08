7e076110e124ec22cc76c5e524495b39da306a9c
guafe <1265480781@qq.com>
2019-08-08 09:29:14 +0800

    小程序模仿

---
+++ 小程序/pages/message/message.js
@@ -0,0 +1,117 @@
+// pages/message/message.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        msgList: [
+            {
+                id: '1',
+                title: "标题 111",
+                date: "2017-12-12",
+                image: "https://m.360buyimg.com/mobilecms/s1125x549_jfs/t14860/139/504394142/191023/ca7c592e/5a2fcd75Ncde0d77d.jpg!q70.jpg",
+                desc: "越努力越幸运。"
+            },
+            {
+                id: '2',
+                title: "标题 222",
+                date: "2017-12-12",
+                image: "https://img1.360buyimg.com/da/jfs/t15013/308/338076309/180567/6cd6603d/5a2a466cNff568379.jpg",
+                desc: "越努力越幸运。"
+            },
+            {
+                id: '3',
+                title: "标题 333",
+                date: "2017-12-12",
+                image: "https://m.360buyimg.com/mobilecms/s1125x549_jfs/t14023/275/1983074516/150155/84e8e488/5a2f7a69N4292c954.jpg!q70.jpg",
+                desc: "越努力越幸运。"
+            },
+            {
+                id: '4',
+                title: "标题 444",
+                date: "2017-12-12",
+                image: "https://m.360buyimg.com/mobilecms/s1125x549_jfs/t15328/245/519883024/134960/eedef659/5a2f8103Nb5f6cbab.jpg!q70.jpg",
+                desc: "越努力越幸运。"
+            },
+            {
+                id: '5',
+                title: "标题 555",
+                date: "2017-12-12",
+                image: "https://m.360buyimg.com/mobilecms/s1125x549_jfs/t15328/245/519883024/134960/eedef659/5a2f8103Nb5f6cbab.jpg!q70.jpg",
+                desc: "越努力越幸运。"
+            },
+            {
+                id: '6',
+                title: "标题 666",
+                date: "2017-12-12",
+                image: "https://m.360buyimg.com/mobilecms/s1125x549_jfs/t15328/245/519883024/134960/eedef659/5a2f8103Nb5f6cbab.jpg!q70.jpg",
+                desc: "越努力越幸运。"
+            },
+            {
+                id: '7',
+                title: "标题 777",
+                date:"2017-12-12",
+                image:"https://m.360buyimg.com/mobilecms/s1125x549_jfs/t15328/245/519883024/134960/eedef659/5a2f8103Nb5f6cbab.jpg!q70.jpg",
+                desc:"越努力越幸运。"
+            },
+
+        ]
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
