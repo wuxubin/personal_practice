5782e64d1d56c6d2e310c692c0af34ecfa2abc37
wuxubin <43275249+wuxubin@users.noreply.github.com>
2019-08-11 21:49:44 +0800

    Update MyTitle.js

--- bilibili小程序/components/MyTitle/MyTitle.js
+++ bilibili小程序/components/MyTitle/MyTitle.js
@@ -1,31 +1,23 @@
-App({
-
+// components/Mytitle/MyTitle.js
+Component({
   /**
-   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   * 组件的属性列表
    */
-  onLaunch: function () {
-    console.log('gggg')
+  properties: {
 
   },
 
   /**
-   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   * 组件的初始数据
    */
-  onShow: function (options) {
-    
+  data: {
+
   },
 
   /**
-   * 当小程序从前台进入后台，会触发 onHide
+   * 组件的方法列表
    */
-  onHide: function () {
-    
-  },
+  methods: {
 
-  /**
-   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
-   */
-  onError: function (msg) {
-    
   }
 })
