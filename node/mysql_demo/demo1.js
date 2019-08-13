4e2112be281cbdcde89db18df7a4f92f9730a3b1
wuxubin <43275249+wuxubin@users.noreply.github.com>
2019-08-13 23:10:20 +0800

    Update demo1.js

--- node/mysql_demo/demo1.js
+++ node/mysql_demo/demo1.js
@@ -8,10 +8,10 @@ var connection = mysql.createConnection({
   database: 'mywxb'
 });
 
-// 2. 连接数据库 打开冰箱门
+// 2. 连接数据库 
 connection.connect();
 
-// 3. 执行数据操作 把大象放到冰箱
+// 3. 执行数据操作 
 connection.query('SELECT * FROM `websites`', function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results);
@@ -22,5 +22,5 @@ connection.query('INSERT INTO websites VALUES(7, "admin", "123456",222,"cn")', f
   console.log('The solution is: ', results);
 });
 
-// 4. 关闭连接 关闭冰箱门
+// 4. 关闭连接 
 connection.end();
